6582ae704e3a6cc4e6a29258594ee16973f00572
Jonas Wisplinghoff <mail@jonaswisplinghoff.de>
2014-05-11 11:27:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Snack Attack - DIY Fastfood WBT
+
+Web based training about cheap, simple and healthy cooking for "Lernen und Lehren mit neuen Medien", Medieninformatik Master SS2014, Technische Hochschule Mittelhessen.
+
+## Installation
+
+* clone repository
+* install dependencies using [Bower](http://bower.io/): `bower install`
---
+++ app.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage(
+    	{
+        anchors: ['firstPage', 'secondPage', 'thirdPage'],
+        verticalCentered: true,
+        resize : true,
+        slidesColor : ['#ccc', '#fff', '#ccc'],
+        scrollingSpeed: 700,
+        easing: 'easeInQuart',
+        navigation: true,
+        navigationPosition: 'right',
+        loopBottom: false,
+        loopTop: false,
+        menu: 'myMenu',
+        loopHorizontal: true,
+        autoScrolling: true,
+        scrollOverflow: false,
+        css3: false,
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        keyboardScrolling: true,
+        touchSensitivity: 15,
+        continuousVertical: false,
+        animateAnchor: true,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    });
+
+	$("#section1button").click(function(){
+		$("#section1button").prop('disabled', true).text('Hier warst du schon');
+
+		$.fn.fullpage.moveSectionDown();
+	});
+
+	$("#section2button").click(function(){
+		$("#section2button").prop('disabled', true).text('Hier warst du schon');
+
+		$.fn.fullpage.moveSectionDown();
+	});
+
+	$("#section3button").click(function(){
+		$("#section3").addClass("has-success");
+		$("#section3button").prop('disabled', true).text('Yeah, fertig!');
+        $("#section3button").addClass("btn-success");
+
+	})
+
+});
