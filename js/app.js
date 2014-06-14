4c144b517fb5aa4eb255c234e3e7021e550b91b3
Jonas Wisplinghoff <mail@jonaswisplinghoff.de>
2014-06-14 20:25:16 +0200

    eleminated fixed widths

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
         touchSensitivity: 15,
         continuousVertical: false,
         animateAnchor: true,
+        navigationTooltips: ['Start', 'Basics', 'Lebensmittel', 'Test', 'Rezepte', 'Credits'],
 
         //events
         onLeave: function(index, nextIndex, direction){},
@@ -31,24 +32,4 @@ $(document).ready(function() {
         afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
         onSlideLeave: function(anchorLink, index, slideIndex, direction){}
     });
-
-	$("#section1button").click(function(){
-		$("#section1button").prop('disabled', true).text('Hier warst du schon');
-
-		$.fn.fullpage.moveSectionDown();
-	});
-
-	$("#section2button").click(function(){
-		$("#section2button").prop('disabled', true).text('Hier warst du schon');
-
-		$.fn.fullpage.moveSectionDown();
-	});
-
-	$("#section3button").click(function(){
-		$("#section3").addClass("has-success");
-		$("#section3button").prop('disabled', true).text('Yeah, fertig!');
-        $("#section3button").addClass("btn-success");
-
-	})
-
 });
